5b54344943f52aa6d9ccac6aa29b0a76df108133
kangduan <duanea0606@gmail.com>
2016-11-03 02:05:14 +0000

    dskflj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,13 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-<script type="text/javascript" src="/javascript/jquery.1.8.3.min.js"></script>
-<script type="text/javascript" src="/javascript/bootstrap.js"></script>
-<script type="text/javascript" src="/javascript/jquery-scrolltofixed.js"></script>
-<script type="text/javascript" src="/javascript/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="/javascript/jquery.isotope.js"></script>
-<script type="text/javascript" src="/javascript/wow.js"></script>
-<script type="text/javascript" src="/javascript/classie.js"></script>
-<script type="text/javascript" src="/javascript/application.js"></script>
-<script type="text/javascript" src="/javascript/home.coffee"></script>
-<script src="contactform/contactform.js"></script
+<script type="text/javascript" src="assets/javascripts/jquery.1.8.3.min.js"></script>
+<script type="text/javascript" src="assets/javascripts/bootstrap.js"></script>
+<script type="text/javascript" src="assets/javascripts/jquery-scrolltofixed.js"></script>
+<script type="text/javascript" src="assets/javascripts/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="assets/javascripts/jquery.isotope.js"></script>
+<script type="text/javascript" src="assets/javascripts/wow.js"></script>
+<script type="text/javascript" src="assets/javascripts/classie.js"></script>
+<script src="contactform/contactform.js"></script>
