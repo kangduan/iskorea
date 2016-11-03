c10e34d46fd5db96aba432f6e472087f13e4b1d1
kangduan <duanea0606@gmail.com>
2016-11-03 01:59:04 +0000

    dskflj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+<script type="text/javascript" src="/javascript/jquery.1.8.3.min.js"></script>
+<script type="text/javascript" src="/javascript/bootstrap.js"></script>
+<script type="text/javascript" src="/javascript/jquery-scrolltofixed.js"></script>
+<script type="text/javascript" src="/javascript/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="/javascript/jquery.isotope.js"></script>
+<script type="text/javascript" src="/javascript/wow.js"></script>
+<script type="text/javascript" src="/javascript/classie.js"></script>
+<script type="text/javascript" src="/javascript/application.js"></script>
+<script type="text/javascript" src="/javascript/home.coffee"></script>
+<script src="contactform/contactform.js"></script
