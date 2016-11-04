69dfd8105fc0f1499c58849b5d898a14dd727304
kangduan <duanea0606@gmail.com>
2016-11-04 04:29:23 +0000

    dfklj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-= require_tree ../../assets/javascripts
+//= require_tree ../../assets/javascripts
+*= require_tree ../../assets/javascripts
